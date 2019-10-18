22c56288e8efef330add21038f38d3056fd02bbd
ZoomZoom123 <56050485+ZoomZoom123@users.noreply.github.com>
2019-10-18 16:19:58 -0700

    Create TypeConversionDemo3.cs

---
+++ TypeConversionDemo3.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace TypeConversion
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = "1234";
+            int i = Convert.ToInt32(number);
+            Console.WriteLine(i);
+        }
+    }
+}
