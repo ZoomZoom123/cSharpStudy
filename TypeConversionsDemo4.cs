5d1757106436b930c737e74c946c6cc28b64856e
ZoomZoom123 <56050485+ZoomZoom123@users.noreply.github.com>
2019-10-19 15:16:27 -0700

    Create TypeConversionsDemo4.cs

---
+++ TypeConversionsDemo4.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace TypeConversion
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to a byte.");
+            }
+
+        }
+    }
+}
