c5dde3b3d29ac84e597e6725701fa9f590afaeff
ZoomZoom123 <56050485+ZoomZoom123@users.noreply.github.com>
2019-10-19 21:55:48 -0700

    Create RandomClass01.cs

---
+++ RandomClass01.cs
@@ -0,0 +1,15 @@
+using System;
+namespace CSharpFundamentals
+{
+    public class RandomClass
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine(random.Next(1, 10));
+            }
+        }
+    }
+}
