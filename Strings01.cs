b921d33b7900e6b8331af9869c8b9785e916cf01
ZoomZoom123 <56050485+ZoomZoom123@users.noreply.github.com>
2019-10-19 18:27:23 -0700

    Create Strings01.cs

---
+++ Strings01.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace CSharpFundamentals
+{
+    public class Strings
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Mino";
+            var lastName = "Smith";
+
+            var fullName = firstName + " " + lastName;
+
+            var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
+
+            var names = new string[3] { "John", "Jack", "Mary" };
+            var formattedNames = string.Join(",", names);
+            
+            Console.WriteLine(formattedNames);
+
+            var text = @"Hi John
+Look into the following paths
+c:\folder1\folder2
+c:\folder3\folder4";
+            Console.WriteLine(text);
+        }
+    }
+}
